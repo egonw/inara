698c58b5965a0c5858b2ebf8e67540be817f3b45
Egon Willighagen <egon.willighagen@gmail.com>
2023-03-13 11:21:55 +0100

    Hooked in CiTO extraction

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -98,6 +98,8 @@ for format in $(printf "%s" "$outformats" | sed -e 's/,/ /g'); do
     # Note that the output file must be defined in the format's defaults file.
     /usr/local/bin/pandoc \
 	      --data-dir="$OPENJOURNALS_PATH"/data \
+        --lua-filter="$OPENJOURNALS_PATH"/data/filters/extract-cito.lua \
+        --citeproc \
         --defaults=shared \
         --defaults="${format}" \
         --defaults="$OPENJOURNALS_PATH"/"${JOURNAL}"/defaults.yaml \
